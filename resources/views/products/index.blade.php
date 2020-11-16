61562fa238fbeb92092d08b0f660374ace986487
Jerald Pamugas <jeraldpamugas11@gmail.com>
2020-11-16 16:05:22 +0800

    Update Index in pagination

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,7 +18,7 @@
         </div>
     @endif
    
-    <table class="table table-bordered">
+    <table class="table table-striped">
         <tr>
             <th>No</th>
             <th>Name</th>
